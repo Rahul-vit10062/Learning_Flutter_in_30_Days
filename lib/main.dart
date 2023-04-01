06b4d2b318f729112150c388bf064b37a3843d25
Rahul Kumar <rahulkumar2022@vitbhopal.ac.in>
2023-04-01 22:54:20 +0530

    Day-3_Login_Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/Home_Page.dart';
+import 'package:flutter_catalog/Pages/Home_Page.dart';
+import 'package:flutter_catalog/Pages/Login_Page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,19 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+
+      darkTheme: ThemeData(brightness: Brightness.dark),
+
+      routes: {
+        "/":(context) => LoginPage(),
+        "/HomePage":(context) => HomePage(),
+      },
     );
   }
 }
